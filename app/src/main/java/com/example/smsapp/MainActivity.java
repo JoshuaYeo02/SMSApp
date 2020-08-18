5fe7a05942956d8de1619d972cf763dc0ca685c7
Joshua <joshauyeo99@gmail.com>
2020-08-17 17:02:41 -0700

    Added Basic Sending of SMS from Text box

--- app/src/main/java/com/example/smsapp/MainActivity.java
+++ app/src/main/java/com/example/smsapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.smsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNumber;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS,
+                Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, PackageManager.PERMISSION_GRANTED);
+
+        editTextMessage = findViewById(R.id.editText);
+        editTextNumber = findViewById(R.id.editTextNumber);
+
+    }
+
+    public void sendSMS(View view){
+
+        String message = editTextMessage.getText().toString();
+        String number = editTextNumber.getText().toString();
+
+        SmsManager mySmsManager = SmsManager.getDefault();
+        mySmsManager.sendTextMessage(number, null, message, null, null);
+
     }
 }
