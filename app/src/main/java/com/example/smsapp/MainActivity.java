9bb3ea78b0807146a0403572863a5099c5f18de3
Joshua <joshauyeo99@gmail.com>
2020-08-20 17:01:40 -0700

    Added the reading of the most recent SMS and display through the Text View

--- app/src/main/java/com/example/smsapp/MainActivity.java
+++ app/src/main/java/com/example/smsapp/MainActivity.java
@@ -5,26 +5,33 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextNumber;
     private EditText editTextMessage;
 
+    private TextView myTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //requestion permissions
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS,
                 Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, PackageManager.PERMISSION_GRANTED);
-
+        //Sending Text
         editTextMessage = findViewById(R.id.editText);
         editTextNumber = findViewById(R.id.editTextNumber);
+        //Reading text
+        myTextView = findViewById(R.id.textView);
 
     }
 
@@ -35,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
 
         SmsManager mySmsManager = SmsManager.getDefault();
         mySmsManager.sendTextMessage(number, null, message, null, null);
+
+    }
+    public void readSMS(View view){
+
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms"),
+                null,null,null,null);
+        cursor.moveToFirst();
+
+        myTextView.setText(cursor.getString(12));
+
 
     }
 }
